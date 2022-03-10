1f17c57fefcf2a9e3bd113bd6859134bdd78a1e6
subin sabu <subinsabu148@gmail.com>
2022-03-10 17:13:04 +0530

    deploy-second

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Typewriter } from "react-simple-typewriter";
 
 const WeatherApp = () => {
   const [data, setData] = useState([]);
@@ -25,10 +26,24 @@ const WeatherApp = () => {
       });
   };
   return (
-    <div className={`bg-green-400 dark:bg-black h-[100vh] w-screen overflow-hidden ${darkMode && "dark"}`}>
-      <div className="dark:bg-[#012346] h-[100vh] w-screen">
-      <div className="flex place-content-evenly">
-        <div className=" h[100vh]  mt-8 md:mt-36">
+    <div className={`bg-green-400 dark:bg-black h-[100vh] w-screen bg-cover   ${darkMode && "dark"}`}>
+      <div className="dark:bg-[#012346] h-[100vh]">
+       <div className="flex justify-center items-center">
+       <h1 className="text-2xl text-[#012346] dark:text-white font-bold ">
+          <Typewriter
+          loop
+          cursor
+          cursorStyle="_"
+          typeSpeed={70}
+          delaySpeed={1000}
+          deleteSpeed={50}
+          words={['Weather App','Developed by Subin Sabu']}
+          w
+          />
+        </h1>
+       </div>
+      <div className="flex place-content-evenly mt-12">
+        <div className="   mt-8 md:mt-36">
           <h1 className="text-2xl md:text-5xl text-gray-700 dark:text-white font-bold ">
            weather_app
           </h1>
@@ -50,12 +65,12 @@ handleClick()
      <div>
         <div className="mt-12">
           <div className="flex justify-center items-center">
-            <div className=" w-11/12 md:w-8/12 flex flex-row justify-center items-center bg-white dark:bg-black outline-none rounded-full h-12 dark:text-white">
+            <div className="  flex flex-row justify-center items-center w-[400px] md:w-10/12 bg-white dark:bg-black outline-none rounded-full h-12 dark:text-white">
               <i className="fa-solid fa-magnifying-glass fa-xl ml-5 text-gray-400 font-bold"> </i>
               <input
                 type="text"
                 placeholder="search for location"
-                className="outline-none text-2xl  pl-5 w-screen rounded-full h-12 bg-white dark:bg-black"
+                className="outline-none text-2xl w-screen rounded-full h-12 p-5 bg-white dark:bg-black"
                 value={data}
                 onChange={(e) => setData(e.target.value)}
 
@@ -77,7 +92,8 @@ handleClick()
         </div>
       }
       {response && (
-        <div className=" mt-12 flex flex-col justify-center items-center md:flex-col md:place-content-between lg:flex-col  bg-blue-800 dark:bg-black w-full md:w-8/12  rounded-xl md:ml-44 lg:ml-56 xl:ml-80 xl:flex-row">
+       <div className="flex justify-center items-center">
+          <div className=" mt-12 flex flex-col justify-center items-center md:flex-col md:place-content-between lg:flex-row  bg-blue-800 dark:bg-black w-full md:full lg:w-10/12 md:rounded-xl xl:flex-row">
           <div >
             <div>
               <h1 className="text-gray-400 text-center md:text-left font-bold md:ml-12 text-3xl mt-6">
@@ -169,13 +185,9 @@ handleClick()
             </div>
           </div>
         </div>
+       </div>
+       
       )}
-      <a href="https://github.com/dev-subin">
-        {" "}
-        <p className="text-xl text-gray-700 text-center mt-12">
-          Developed by <span className="underline">Subin Sabu</span>
-        </p>
-      </a>
     </div>
     </div>
   );
